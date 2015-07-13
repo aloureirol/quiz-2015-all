bf8501be0a516e2527c94ef6ea0e7d9e79c6fb85
aloureirol <aloureirol@gmail.com>
2015-07-13 12:39:49 +0200

    Despliegue DB en Heroku

---
+++ models/models.js
@@ -0,0 +1,50 @@
+//models.js construye la DB importando el modelo de quiz.js
+//sequelize.sync() construye la DB según define el modelo
+
+var path = require('path');
+
+//Postgress DATABASE_URL = postgress://user:passwd@host:port/database
+//SQLite    DATABASE_URL = sqlite://:@:/
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name	= (url[6]||null);
+var user	= (url[2]||null);
+var pwd		= (url[3]||null);
+var protocol	= (url[1]||null);
+var dialect	= (url[1]||null);
+var port	= (url[5]||null);
+var host	= (url[4]||null);
+var storage	= process.env.DATABASED_STORAGE;
+
+//Cargar modelo ORM
+var Sequelize = require('sequelize');
+
+//Usar DB SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+		{dialect:	protocol,
+		protocol:	protocol,
+		port:		port,
+		host:		host,
+		storage:	storage, //solo SQLite (.env)
+		omitNull:	true	//Solo Postgtree
+		}
+		);
+
+//Imporar la definición de la table Quiz
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+exports.Quiz = Quiz; //exportar definición de tabla Quiz
+
+//creamos e inicializamos la tabla de preguntas en DB
+sequelize.sync().success(function(){
+	//succes(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().success(function(count){
+		if(count===0){ //La tabla se inicializa solo si está vacía
+			Quiz.create({pregunta: 'Capital de Italia',
+				respuesta: 'Roma'
+			})
+			.success(function(){console.log('DB Inicializada');
+					console.log('servidor iniciado en el puerto 5000');});
+		};	
+	});
+});
