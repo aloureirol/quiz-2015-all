8d48ada9b31dca1b7748c2238e7e2b9e1829b142
aloureirol <aloureirol@gmail.com>
2015-07-16 11:41:26 +0200

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -37,18 +37,18 @@ exports.index=function(req,res){
 	if(req.query.search !== undefined){		//Comprobamos si es la carga incial o 
 																				//o una busqueda
 		models.Quiz.findAll({where:["pregunta like ?",'%'+req.query.search.replace(" ","%")+'%']}).then(function(quizes){
-		res.render('quizes/index',{quizes: quizes});
+		res.render('quizes/index',{quizes: quizes, errors: []});
 	})
 }else{
 		models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index',{quizes: quizes});
+		res.render('quizes/index',{quizes: quizes, errors: []});
 	})
 }
 };
 
 //GET /quizes/:id
 exports.show=function(req,res){
-		res.render('quizes/show',{quiz: req.quiz});
+		res.render('quizes/show',{quiz: req.quiz, errors: []});
 };
 
 //GET /quizes/:id/answer
@@ -57,5 +57,35 @@ exports.answer=function(req,res){
 	if(req.query.respuesta === req.quiz.respuesta){
 		resultado = 'Correcto';
 	}
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', {quiz: req.quiz, 
+		respuesta: resultado,
+		errors: []});
+};
+
+//GET /quizes/new
+exports.new = function(req,res){
+	var quiz = models.Quiz.build( //build propiedad de sequelize crea objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+	res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+//POST /quizes/create
+exports.create = function(req,res){
+	var quiz = models.Quiz.build (req.body.quiz);
+	
+	quiz
+		.validate()
+		.then(
+			function(err){
+				if(err){
+					res.render('quizes/new', {quiz: quiz, errors: err.errors});
+				}else{
+		//guarda en DB los campos pregunta y respuesta de quiz
+				quiz
+				.save({fields: ["pregunta","respuesta"]})
+				.then(function(){res.redirect('/quizes')})	//Redirección URL relativo, lista de preguntas
+			}
+		}
+	);
 };
